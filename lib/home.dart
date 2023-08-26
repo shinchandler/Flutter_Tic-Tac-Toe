95eeb69cb8df67dd0b34b1206a3ef3c9d838106e
shinchandler <pranavrishi8@gmail.com>
2023-08-26 16:42:26 +0530

    rewritten code

--- lib/home.dart
+++ lib/home.dart
@@ -8,37 +8,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> displayXO = ['','','','','','','','',''];
+  bool oTurn = true;
+  List<String> displayXO = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    ''
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          backgroundColor: Colors.grey[800],
-          body: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            itemCount: 9,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  _tapped(index);
-                },
-                child: Container(
-                  decoration: BoxDecoration(border: Border.all(color: Colors.grey.shade700)),
-                  child: Center(
-                      child: Text(
-                    displayXO[index],
-                    style: const TextStyle(fontSize: 40, color: Colors.white),
-                  )),
-                ),
-              );
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: GridView.builder(
+        itemCount: 9,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              _onTapped(index);
             },
-          )),
+            child: Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Center(
+                    child: Text(
+                  displayXO[index],
+                  style: const TextStyle(fontSize: 40, color: Colors.white),
+                ))),
+          );
+        },
+      ),
     );
   }
 
-  void _tapped (int index) {
+  void _onTapped(index) {
     setState(() {
-      displayXO[index] ='O';
+      if (oTurn) {
+        displayXO[index] = 'O';
+      } else {
+        displayXO[index] = 'X';
+      }
+      oTurn = !oTurn;
     });
   }
 }

b7abb1e672a4bc397dcf252cd251d290f5ee8d56
shinchandler <pranavrishi8@gmail.com>
2023-08-29 21:05:10 +0530

    check and display winner

--- lib/home.dart
+++ lib/home.dart
@@ -56,12 +56,44 @@ class _HomePageState extends State<HomePage> {
       }
       oTurn = !oTurn;
       _checkWinner();
+      
     });
   }
-}
 
-void _checkWinner() {
-  _display();
-}
+  void _checkWinner() {
+    if (displayXO[0] == displayXO[1] && displayXO[0] == displayXO[2] && displayXO[0] != '') {
+      _display(displayXO[0]);
+    }
+    if (displayXO[3] == displayXO[4] && displayXO[3] == displayXO[5] && displayXO[3] != '') {
+      _display(displayXO[3]);
+    }
+    if (displayXO[6] == displayXO[7] && displayXO[6] == displayXO[8] && displayXO[6] != '') {
+      _display(displayXO[6]);
+    }
+    if (displayXO[0] == displayXO[3] && displayXO[0] == displayXO[6] && displayXO[0] != '') {
+      _display(displayXO[0]);
+    }
+    if (displayXO[1] == displayXO[4] && displayXO[1] == displayXO[7] && displayXO[1] != '') {
+      _display(displayXO[1]);
+    }
+    if (displayXO[2] == displayXO[5] && displayXO[2] == displayXO[8] && displayXO[2] != '') {
+      _display(displayXO[2]);
+    }
+    if (displayXO[0] == displayXO[4] && displayXO[0] == displayXO[8] && displayXO[0] != '') {
+      _display(displayXO[0]);
+    }
+    if (displayXO[2] == displayXO[4] && displayXO[2] == displayXO[6] && displayXO[2] != '') {
+      _display(displayXO[2]);
+    }
+  }
 
-void _display() {}
+  void _display(String winner) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Winner is :$winner'),
+          );
+        });
+  }
+}
